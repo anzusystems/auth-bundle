df29e87d5649cc8e65f0333e451b1b7c3378fd21
gabrielzigo <gabriel.zigo@petitpress.sk>
2026-07-22 13:27:58 +0200

    Import the notifier interface from Contracts in the noop implementation

--- src/Domain/PersonalAccessToken/Notification/NoopPersonalAccessTokenExpiryNotifier.php
+++ src/Domain/PersonalAccessToken/Notification/NoopPersonalAccessTokenExpiryNotifier.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AnzuSystems\AuthBundle\Domain\PersonalAccessToken\Notification;
 
+use AnzuSystems\AuthBundle\Contracts\PersonalAccessTokenExpiryNotifierInterface;
 use AnzuSystems\AuthBundle\Entity\AbstractPersonalAccessToken;
 
 final readonly class NoopPersonalAccessTokenExpiryNotifier implements PersonalAccessTokenExpiryNotifierInterface
